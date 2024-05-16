20f7371f28907f603b55e0ca0e6d3ec205c28e7d
Mahdi Islam Pranto <74008896+Mahdi-Pranto@users.noreply.github.com>
2024-05-17 02:22:18 +0600

    first com

---
+++ basc_concepts.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "mahdi islam";
+  int id = 20101326;
+
+  List<String> list = ["a", "b", "c"];
+  Map<String, dynamic> getAll = {"name": name, "id": id, "list": list};
+  print(getAll);
+}
