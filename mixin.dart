44dcf42d3e6ed387d9bfc2c128090842e8aef90e
Mahdi-Pranto <mahdiprantoblog@gmail.com>
2024-06-02 13:58:47 +0600

    abstract & implements & mixin

---
+++ mixin.dart
@@ -0,0 +1,12 @@
+class Animal with Legs, Color {
+  void live() {}
+}
+
+// mixin class's methods can be use in any classes
+mixin Legs {
+  void leggi() {}
+}
+
+mixin Color {
+  void colors() {}
+}
