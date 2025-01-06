97705be80c9cc0206e75c4997140c3e44c680822
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2025-01-06 23:47:00 +0600

    json to map convertion

---
+++ JsonToMap.dart
@@ -0,0 +1,177 @@
+import 'dart:convert';
+
+String jsonStringResponse = '''
+
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  }
+
+''';
+
+
+void main() {
+  // convert json string to map
+
+  Map<String, dynamic> userMap = json.decode(jsonStringResponse);
+  print("Full API Response::::::::::::::::::::::::");
+  print(userMap);
+
+  // print user detais using map
+  print("User details using Map:::::::::::::::::::::::::::");
+  print('Name: ${userMap['name']}');
+  print('Phone: ${userMap['phone']}');
+  print('Address: ${userMap['address']['street']}');
+  print('Company: ${userMap['company']['name']}');
+
+// print user details using User class Model
+  print("User details using User Class Model:::::::::::::::::::::::::::");
+
+  // create user object
+  UserClass user = UserClass.fromJson(userMap);
+  print("User name: ${user.name}");
+  print("User phone: ${user.phone}");
+  print("User address: ${user.address?.street}");
+  print("User company: ${user.company?.name}");
+}
+
+class UserClass {
+  int? id;
+  String? name;
+  String? username;
+  String? email;
+  Address? address;
+  String? phone;
+  String? website;
+  Company? company;
+
+  UserClass(
+      {this.id,
+      this.name,
+      this.username,
+      this.email,
+      this.address,
+      this.phone,
+      this.website,
+      this.company});
+
+  UserClass.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    username = json['username'];
+    email = json['email'];
+    address =
+        json['address'] != null ? new Address.fromJson(json['address']) : null;
+    phone = json['phone'];
+    website = json['website'];
+    company =
+        json['company'] != null ? new Company.fromJson(json['company']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['username'] = this.username;
+    data['email'] = this.email;
+    if (this.address != null) {
+      data['address'] = this.address!.toJson();
+    }
+    data['phone'] = this.phone;
+    data['website'] = this.website;
+    if (this.company != null) {
+      data['company'] = this.company!.toJson();
+    }
+    return data;
+  }
+}
+
+class Address {
+  String? street;
+  String? suite;
+  String? city;
+  String? zipcode;
+  Geo? geo;
+
+  Address({this.street, this.suite, this.city, this.zipcode, this.geo});
+
+  Address.fromJson(Map<String, dynamic> json) {
+    street = json['street'];
+    suite = json['suite'];
+    city = json['city'];
+    zipcode = json['zipcode'];
+    geo = json['geo'] != null ? new Geo.fromJson(json['geo']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['street'] = this.street;
+    data['suite'] = this.suite;
+    data['city'] = this.city;
+    data['zipcode'] = this.zipcode;
+    if (this.geo != null) {
+      data['geo'] = this.geo!.toJson();
+    }
+    return data;
+  }
+}
+
+class Geo {
+  String? lat;
+  String? lng;
+
+  Geo({this.lat, this.lng});
+
+  Geo.fromJson(Map<String, dynamic> json) {
+    lat = json['lat'];
+    lng = json['lng'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['lat'] = this.lat;
+    data['lng'] = this.lng;
+    return data;
+  }
+}
+
+class Company {
+  String? name;
+  String? catchPhrase;
+  String? bs;
+
+  Company({this.name, this.catchPhrase, this.bs});
+
+  Company.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    catchPhrase = json['catchPhrase'];
+    bs = json['bs'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['catchPhrase'] = this.catchPhrase;
+    data['bs'] = this.bs;
+    return data;
+  }
+}
