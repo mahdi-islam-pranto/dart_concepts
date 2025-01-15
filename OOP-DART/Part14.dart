f4f67c8949b021487ca426ac1d591c3181d14558
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2025-01-15 12:01:19 +0600

    more class concepts

--- OOP-DART/Part14.dart
+++ OOP-DART/Part14.dart
@@ -1,27 +1,18 @@
+// Private class
 class _MyClass {
+  String firstName = 'Rabbil';
 
-  String firstName='Rabbil';
-
-  void addTwo(){
-    var num1=10;
-    var num2=20;
-    print(num2+num1);
+  void addTwo() {
+    var num1 = 10;
+    var num2 = 20;
+    print(num2 + num1);
   }
 }
 
-class ABC extends _MyClass{
+class ABC extends _MyClass {}
 
-}
-
-
-
-
-
-
-void main(){
-
-  _MyClass myObj= _MyClass();
+void main() {
+  _MyClass myObj = _MyClass();
   myObj.addTwo();
   print(myObj.firstName);
-
 }
